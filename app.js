06af303e716794d820617380e9ffcfb70fafcf9b
Krutpong Khunthep <krutpong@gmail.com>
2023-10-01 14:07:40 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const axios = require('axios');
+const fs = require('fs');
 
 const app = express();
 const PORT = 8088;
 const MAX_CONCURRENT_BROWSERS = 2;
-const VIEWPORT_WIDTH = 1920; // กำหนด Viewport Width
-const VIEWPORT_HEIGHT = 1080; // กำหนด Viewport Height
+const VIEWPORT_WIDTH = 1920;
+const VIEWPORT_HEIGHT = 1080;
 
 // อ่าน User-Agent จากไฟล์ JSON
 const userAgentsData = fs.readFileSync('useragents.json', 'utf8');
@@ -36,18 +36,8 @@ async function createBrowserInstance() {
 app.get('/', async (req, res) => {
   const url = req.query.url;
 
-  if (!url) {
-    return res.status(400).send('URL parameter is required.');
-  }
-
-  try {
-    const response = await axios.head(url);
-    if (response.status !== 200) {
-      return res.status(400).send('The specified URL is not accessible. Status code: ' + response.status);
-    }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send('An error occurred while fetching the URL.');
+  if (!url || !isValidUrl(url)) {
+    return res.status(400).send('Invalid URL format.');
   }
 
   let browser;
@@ -63,7 +53,7 @@ app.get('/', async (req, res) => {
 
     page = await browser.newPage();
     await page.setViewport({ width: VIEWPORT_WIDTH, height: VIEWPORT_HEIGHT });
-    await page.setUserAgent(getRandomUserAgent()); // กำหนด User-Agent สุ่ม
+    await page.setUserAgent(getRandomUserAgent());
 
     await page.goto(url);
 
@@ -91,6 +81,11 @@ app.get('/', async (req, res) => {
     }
   }
 });
+
+function isValidUrl(inputUrl) {
+  const urlPattern = /^(http|https):\/\/[^ "]+$/;
+  return urlPattern.test(inputUrl);
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
