6f5992b6c6efc561a9920ab458e307f3f0287f21
Krutpong Khunthep <krutpong@gmail.com>
2023-10-03 00:23:00 +0700

    update

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ const puppeteer = require("puppeteer");
 const app = express();
 const port = 8088;
 
-app.get("/scrape", async (req, res) => {
+app.get("/html", async (req, res) => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true, // ให้เป็น true เพื่อใช้โหมด headless
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // เพิ่มอาร์กิวเมนต์นี้เพื่อป้องกันปัญหาในการใช้งาน Docker
+      ignoreHTTPSErrors: true, // อัพเกรด Puppeteer ที่มีการอ้างอิงไปยังแหล่ง HTTPS
+    });
+
     const page = await browser.newPage();
-    
+    await page.setCacheEnabled(false); // ปิดการใช้งานแคชบนหน้า Puppeteer
+    await page.setDefaultNavigationTimeout(0); // ไม่มี timeout ในการโหลดหน้าเว็บ
+
     const url = req.query.url;
     if (!url) {
       throw new Error("Missing 'url' parameter");
     }
-    
+
     await page.goto(url);
     const content = await page.content();
-    
+
     await browser.close();
-    
+
     res.send(content);
   } catch (error) {
     res.status(500).json({ error: error.message });
