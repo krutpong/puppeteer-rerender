287691dd6a09940834d7247ef2fd8a763020a471
Krutpong Khunthep <krutpong@gmail.com>
2023-10-03 00:29:02 +0700

    update

--- server.js
+++ server.js
@@ -6,19 +6,29 @@ const port = 8088;
 
 app.get("/html", async (req, res) => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: "/usr/bin/chromium-browser",
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      ignoreHTTPSErrors: true,
+    });
+
     const page = await browser.newPage();
-    
+    await page.setCacheEnabled(false);
+    await page.setDefaultNavigationTimeout(0);
+
     const url = req.query.url;
     if (!url) {
       throw new Error("Missing 'url' parameter");
     }
-    
+
     await page.goto(url);
     const content = await page.content();
-    
+
     await browser.close();
-    
+
+    // ตั้งค่า header เพื่อรองรับ UTF-8
+    res.header("Content-Type", "text/html; charset=utf-8");
     res.send(content);
   } catch (error) {
     res.status(500).json({ error: error.message });
